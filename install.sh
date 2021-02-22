d11abece6cfcfc5746a8f134fe05b91d625f9ff8
ddefreitas.exe <1959720+darrell-d@users.noreply.github.com>
2021-02-21 21:24:10 -0500

    Adding extra ease to install

--- install.sh
+++ install.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 #/usr/bin/bash
 if [ ! -f ~/.bash_profile_extras ]; then
-	echo"Copying bash profiles"
+	echo "Copying bash profiles"
 	cp ./.bash_profile_extras ~/
 	cp ./.bashrc_extras ~/
+	cp ./.bash_custom ~/
 
 	# Append to `.bash_profile`
 	echo "
-
-	if [ -f ~/.bash_profile_extras ]; then
-		. ~/.bash_profile_extras
-	fi
-	if [ -f ~/.bashrc_extras ]; then
-		. ~/.bashrc_extras
-	fi"  | tee -a ~/.bash_profile
-
-	echo "# configuration for customization options per environment
-
-	alias ..code=''" >> ~/.bash_custom
+if [ -f ~/.bash_profile_extras ]; then
+	. ~/.bash_profile_extras
+fi
+if [ -f ~/.bashrc_extras ]; then
+	. ~/.bashrc_extras
+fi"  | tee -a ~/.bash_profile
 
 	source ~/.bash_profile
 fi
